74192f3c7f0f2520d5ef345ee19c894332fd1da4
vikasjoshis001 <vikasjoshis001@gmail.com>
2022-03-12 15:49:38 +0530

    shutdown restart gestures added

--- Gestures.py
+++ Gestures.py
@@ -5,6 +5,7 @@ import time
 import HandTrackingModule as htm
 from pynput.mouse import Button, Controller as mouseController
 from pynput.keyboard import Key, Controller as keyboardController
+from tkinter import *
 
 mouse = mouseController()
 keyboard = keyboardController()
@@ -14,6 +15,10 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 detector = htm.handDetector()
 frameR = 100
+root = Tk()
+
+def isAccepted():
+    print("yes")
 
 # Coding Gestures...
 while True:
@@ -21,6 +26,10 @@ while True:
     success, img = cap.read()
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img)
+
+    # Create a frame...
+    app = Frame(root, bg="white")
+    app.grid()
 
     if len(lmList) != 0:
         x1 = lmList[8][1:]
@@ -30,22 +39,22 @@ while True:
         cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (255, 255, 255), 2)
 
         # Scrolling Up Down Gesture...
-        if fingers[1] == 1 and fingers[2] == 1:
-            length, img, info = detector.findDistance(8, 12, img)
-            if length <= 30:
-                keyboard.press(Key.up)
-                keyboard.release(Key.up)
-                cv2.circle(img, (info[4], info[5]), 10, (0, 255, 0), cv2.FILLED)
+        # if fingers[1] == 1 and fingers[2] == 1:
+        #     length, img, info = detector.findDistance(8, 12, img)
+        #     if length <= 30:
+        #         keyboard.press(Key.up)
+        #         keyboard.release(Key.up)
 
-            if length > 30:
-                keyboard.press(Key.down)
-                keyboard.release(Key.down)
+            # if length > 30:
+            #     keyboard.press(Key.down)
+            #     keyboard.release(Key.down)
 
         # Exit File Gesture...
-        if fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0 and fingers[2] == 0:
+        if fingers[0] == 0 and fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0 and fingers[2] == 0:
+            print("Exit File Working....")
             with keyboard.pressed(Key.ctrl):
-                keyboard.press("Q")
-                keyboard.release("Q")
+                keyboard.press("S")
+                keyboard.release("S")
 
         # Save File Gesture...
         if fingers[0] == 1 and fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[2] == 0:
